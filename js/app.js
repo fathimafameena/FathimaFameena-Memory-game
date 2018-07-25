2fd69449a2a03568256d864d5ebb6c3ba7109aeb
fathimafameena <fathima.fameena@yahoo.com>
2018-07-24 23:09:30 -0500

    Added event listener for clicking  the cards and checking functionality for not opening more than 2 cards and  when the cards match make the cards open and when it is not a match close the cards

--- js/app.js
+++ js/app.js
@@ -44,8 +44,60 @@
 
     return array;
 	}
+	var allcards=document.querySelectorAll('.card');/*selecting the element with class card*/
+	// console.log(allcards);
+	var opencards=[];/*creating an empty array to  store all open cards*/
 
+    allcards.forEach(function(card)
+	{
+		console.log("the contains open is"+card.classList.contains('open'));
+		card.addEventListener('click',function(event)/*adding a event listener function for clicking the card*/
+		{
+		if(!card.classList.contains('open')&& !card.classList.contains('show')&& !card.classList.contains('match'))/*click only the cards that is closed and donot click the already opened cards*/
+		{
 
+			console.log("the contains open is"+card.classList.contains('open'));
+			console.log(card+" was clicked");
+			opencards.push(card);/*push the cards that is open into the opencards array*/
+			console.log("the card is" +opencards.length);
+			if(opencards.length<2)/*if only one card is open*/
+			{
+				opencards[0].classList.add('open');
+				opencards[0].classList.add('show');
+				console.log("inside lesser than")
+
+			}
+			if(opencards.length==2)/*if two cards are open*/
+			{
+				console.log("inside greter than")
+				opencards[0].classList.add('open','show');	
+				opencards[1].classList.add('open','show');
+
+			if(opencards[0].dataset.card===opencards[1].dataset.card)/*if the cards match make the cards open*/
+			{   
+				console.log("yes it is matched");
+				opencards[0].classList.add('match');
+				opencards[0].classList.add('open');
+				opencards[0].classList.add('show');
+				opencards[1].classList.add('match');
+				opencards[1].classList.add('open');
+				opencards[1].classList.add('show');
+				opencards=[];/*empty the opencards array for next round*/
+			}
+			else/*if the cards did not match close the cards in 1 seconds*/
+			{
+				setTimeout(function()
+				{
+ 					opencards[0].classList.remove('open','show');
+					opencards[1].classList.remove('open','show');
+					opencards=[];
+				},1000);
+
+			}	
+			}
+		}
+		});
+	});
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
