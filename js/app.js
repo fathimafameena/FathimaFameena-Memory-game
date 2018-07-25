8de5cfb02951059563f4368bc555c842bc600748
fathimafameena <fathima.fameena@yahoo.com>
2018-07-24 22:44:07 -0500

    Added the code which dynamically display the cards

--- js/app.js
+++ js/app.js
@@ -1,29 +1,49 @@
 /*
  * Create a list that holds all of your cards
  */
+	var cards=["fa-diamond","fa-diamond",
+            "fa-paper-plane-o","fa-paper-plane-o",
+            "fa-github","fa-github",
+            "fa-apple","fa-apple",
+            "fa-cube","fa-cube",
+            "fa-leaf","fa-leaf",
+            "fa-bicycle","fa-bicycle",
+            "fa-bomb","fa-bomb"];/*created a array of cards*/
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
+	function genarateCard(card)
+    {
+    	console.log("i am called");
+    	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;/*genarating the HTML list dynamically for all cards*/
+    	console.log(`<li class="card"><i class="fa ${card}"></i></li>`);	
+    }   
+/*a function to intialise the game with all intial parameters*/
+    function initGame()
+    {
+    	var deck=document.querySelector('.deck');
+    	var cardHtml=shuffle(cards.map(function(card)/*shuffling the cards everytime  by using the shuffle function*/
+    	{
+         console.log("the card is"+card); 
+           return genarateCard(card);/*passing the array to the genarateCard function to dynamically create a list of cards*/
+    	}));
+   		console.log("i am cardHtml"+cardHtml.join(''));
+    	deck.innerHTML=cardHtml.join('');
+    }
+	initGame();//intialize function is called when the game starts
+	// Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle(array) 
+	{
+    	var currentIndex = array.length, temporaryValue, randomIndex;
+		while (currentIndex !== 0)
+		 {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-    }
+    	}
 
     return array;
-}
+	}
 
 
 /*
