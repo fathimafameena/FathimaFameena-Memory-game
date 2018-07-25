e337ddc3757d96fb8b4ca39aec8623e8c6f47fff
fathimafameena <fathima.fameena@yahoo.com>
2018-07-25 17:35:52 -0500

    1.Responsive.css is added to make it  display in all screen sizes. 2.if the cars do not match it appears red with shake animation (code is added)

--- js/app.js
+++ js/app.js
@@ -10,141 +10,46 @@
             "fa-bicycle","fa-bicycle",
             "fa-bomb","fa-bomb"];/*created a array of cards*/
 
-	function genarateCard(card)
-    {
-    	console.log("i am called");
-    	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;/*genarating the HTML list dynamically for all cards*/
-    	console.log(`<li class="card"><i class="fa ${card}"></i></li>`);	
-    }   
-/*a function to intialise the game with all intial parameters*/
-    function initGame()
-    {
-    	var deck=document.querySelector('.deck');
-    	var cardHtml=shuffle(cards.map(function(card)/*shuffling the cards everytime  by using the shuffle function*/
-    	{
-         console.log("the card is"+card); 
-           return genarateCard(card);/*passing the array to the genarateCard function to dynamically create a list of cards*/
-    	}));
-   		console.log("i am cardHtml"+cardHtml.join(''));
-    	deck.innerHTML=cardHtml.join('');
-    	setMovesInit();/intialize the moves */
-    }
+	
 	initGame();//intialize function is called when the game starts
-	// Shuffle function from http://stackoverflow.com/a/2450976
-	function shuffle(array) 
-	{
-    	var currentIndex = array.length, temporaryValue, randomIndex;
-		while (currentIndex !== 0)
-		 {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    	}
-
-    return array;
-	}
 	var allcards=document.querySelectorAll('.card');/*selecting the element with class card*/
-	// console.log(allcards);
 	var opencards=[];/*creating an empty array to  store all open cards*/
 	var modal = document.getElementById('myModal');/*modal class*/
     var span = document.getElementsByClassName("close")[0];/*close button in modal*/
-	span.onclick = function() 
-	{
-    	modal.style.display = "none";
-	}
-
-// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) 
-	{
-    	if (event.target == modal) 
-    	{
-        	modal.style.display = "none";
-    	}
-	}
-	var stars=document.querySelector('.stars');
+	var stars=document.querySelector('.stars');/* a variable for star class element*/
 	console.log(stars.children);
 	console.log(stars.children[2]);
 	var trackmoves;
     var restart=document.querySelector('.restart');
-	restart.addEventListener('click',function(event)/*restart button code*/
-	{
-		document.location.reload();
-	});
 	var gametime = document.querySelector('.timer'),seconds = 0, minutes = 0,t;
-	timer();/*calling the timer to start when the page intialises*/
-
     allcards.forEach(function(card)
 	{
-		console.log("the contains open is"+card.classList.contains('open'));
+		console.log("the  class contains open is"+card.classList.contains('open'));
 		card.addEventListener('click',function(event)/*adding a event listener function for clicking the card*/
 		{
 		if(!card.classList.contains('open')&& !card.classList.contains('show')&& !card.classList.contains('match'))/*click only the cards that is closed and donot click the already opened cards*/
 		{
-
 			console.log("the contains open is"+card.classList.contains('open'));
 			console.log(card+" was clicked");
 			opencards.push(card);/*push the cards that is open into the opencards array*/
 			console.log("the card is" +opencards.length);
 			if(opencards.length<2)/*if only one card is open*/
 			{
-				opencards[0].classList.add('open');
-				opencards[0].classList.add('show');
-				console.log("inside lesser than")
+				displayOneCards();
 
 			}
 			if(opencards.length==2)/*if two cards are open*/
 			{
-				console.log("inside greter than")
-				opencards[0].classList.add('open','show');	
-				opencards[1].classList.add('open','show');
-
+				displayTwoCards();
 			if(opencards[0].dataset.card===opencards[1].dataset.card)/*if the cards match make the cards open*/
 			{   
-				console.log("yes it is matched");
-				opencards[0].classList.add('match');
-				opencards[0].classList.add('open');
-				opencards[0].classList.add('show');
-				opencards[1].classList.add('match');
-				opencards[1].classList.add('open');
-				opencards[1].classList.add('show');
-				opencards=[];/*empty the opencards array for next round*/
-				incrementMoves();/*increment moves by 1 for each match*/
-    			trackmoves=document.querySelector('.moves').innerText;
-    			console.log("the trackmoves is"+trackmoves);
-    			if(trackmoves==='10')
-    			{
-       				 stars.children[2].remove('fa fa-star');/*if moves is equal 10 remove one perfomance star*/
-    			}
-				if(trackmoves==='15')
-    			{
-        
-        			stars.children[1].remove('fa','fa-star');/*if moves is equal 15 remove two perfomance star*/
-    			}
-				console.log("checking for winner");
-				winning();
+				match();/*call the match function to see if the cards match*/
+				
 			}
 			else/*if the cards did not match close the cards in 1 seconds*/
 			{
-				incrementMoves();/*increment moves by 1 for each  unmatch*/
-    			trackmoves=document.querySelector('.moves').innerText;
-    			console.log("the trackmoves is"+trackmoves);
-    			if(trackmoves==='10')
-    			{
-        			stars.children[2].remove('fa fa-star');/*if moves is equal 10 remove one perfomance star*/
-    			}
-    			if(trackmoves==='15')
-    			{
-      	  			stars.children[1].remove('fa','fa-star');/*if moves is equal 15 remove two perfomance star*/
-    			}
-				setTimeout(function()
-				{
- 					opencards[0].classList.remove('open','show');
-					opencards[1].classList.remove('open','show');
-					opencards=[];
-				},1000);
-
+				unmatch();/*call the unmatch function if the cards didnt match*/
+				
 			}	
 			}
 		}
@@ -168,19 +73,27 @@
 			console.log("Congratulations you won the game in "+document.querySelector('.timer').innerText+"   with "+document.querySelector('.moves').innerText+ "   moves");
  			var performance,displaystars=0;
 			perfomance=document.querySelector('.moves').innerText;
-			if(perfomance>=10)
+			if(perfomance>=25)
 			{
-    			displaystars=2;/*displaying the stars in winning message based on the moves*/
+    			displaystars=1;/*displaying the perfomance star in number  in winning message based on the moves*/
 			}
 			if(perfomance>=15)
 			{
-   				 displaystars=1
+   				 displaystars=2
 			}
 			if(perfomance<10)
 			{
    				 displaystars=3
 			}
+			if(displaystars===1)
+			{
+    		var winmessage=`YOU WON the game with ${displaystars} star and in a Timing of  ${document.querySelector('.timer').innerText}. Try improving your memory`;
+    		}
+    		else
+    		{
     		var winmessage=`CONGRATULATIONS YOU WON the game with ${displaystars} stars and in a Timing of  ${document.querySelector('.timer').innerText}`;
+
+    		}
     		console.log("the winn message is"+winmessage);
     		var modalcontent=document.querySelector('.winningcontent');
     		modalcontent.innerText=winmessage;/*display the message inside modal*/
@@ -189,10 +102,125 @@
 		}
 
 	}
+
+	function genarateCard(card)
+    {
+    	console.log("i am called");
+    	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;/*genarating the HTML list dynamically for all cards*/
+    	console.log(`<li class="card"><i class="fa ${card}"></i></li>`);	
+    }   
+			/*a function to intialise the game with all intial parameters*/
+    function initGame()
+    {
+    	var deck=document.querySelector('.deck');
+    	var cardHtml=shuffle(cards.map(function(card)/*shuffling the cards everytime  by using the shuffle function*/
+    	{
+         console.log("the card is"+card); 
+           return genarateCard(card);/*passing the array to the genarateCard function to dynamically create a list of cards*/
+    	}));
+   		console.log("i am cardHtml"+cardHtml.join(''));
+    	deck.innerHTML=cardHtml.join('');
+    	setMovesInit();/intialize the moves */
+    	timer();/*calling the timer to start when the page intialises*/
+    }
+    // Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle(array) 
+	{
+    	var currentIndex = array.length, temporaryValue, randomIndex;
+		while (currentIndex !== 0)
+		 {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    	}
+
+    return array;
+	}
+
+	function displayOneCards()
+	{
+		opencards[0].classList.add('open');
+		opencards[0].classList.add('show');
+		console.log("inside lesser than")
+
+	}
+    function displayTwoCards()
+    {
+		console.log("inside greter than")
+		opencards[0].classList.add('open','show');	
+		opencards[1].classList.add('open','show');
+	}
+	function match()
+	{
+		console.log("yes it is matched");
+		opencards[0].classList.add('match');
+		opencards[0].classList.add('open');
+		opencards[0].classList.add('show');
+		opencards[1].classList.add('match');
+		opencards[1].classList.add('open');
+		opencards[1].classList.add('show');
+		opencards=[];/*empty the opencards array for next round*/
+		incrementMoves();/*increment moves by 1 for each match*/
+    	trackmoves=document.querySelector('.moves').innerText;
+    	console.log("the trackmoves is"+trackmoves);
+    	if(trackmoves==='15')
+    	{
+       		 stars.children[2].remove('fa fa-star');/*if moves is equal 10 remove one perfomance star*/
+    	}
+		if(trackmoves==='25')
+    	{
+        	stars.children[1].remove('fa','fa-star');/*if moves is equal 15 remove two perfomance star*/
+    	}
+		console.log("checking for winner");
+		winning();
+	}
+	function unmatch()
+	{
+		opencards[0].classList.add('unmatch');
+		opencards[1].classList.add('unmatch');
+		incrementMoves();/*increment moves by 1 for each  unmatch*/
+    	trackmoves=document.querySelector('.moves').innerText;
+    	console.log("the trackmoves is"+trackmoves);
+    	if(trackmoves==='15')
+    	{
+        	stars.children[2].remove('fa fa-star');/*if moves is equal 15 remove one perfomance star*/
+    	}
+    	if(trackmoves==='25')
+    	{
+      	  	stars.children[1].remove('fa','fa-star');/*if moves is equal 25 remove two perfomance star*/
+    	}
+		setTimeout(function()
+		{
+ 			opencards[0].classList.remove('open','show','unmatch');
+			opencards[1].classList.remove('open','show','unmatch');/*make the unmatch cards close in 1 second*/
+			opencards=[];
+		},1000);
+
+	}
 	function modalDisplay()
 	{
     	modal.style.display = "block"
 	}
+
+	span.onclick = function() //close the modal when user clicks it
+	{
+    	modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) 
+	{
+    	if (event.target == modal) 
+    	{
+        	modal.style.display = "none";
+    	}
+	}
+	restart.addEventListener('click',function(event)/*restart button code*/
+	{
+		document.location.reload();
+	});
 	function add() /*function for calculating seconds and minutes*/
 	{
    		seconds++;
